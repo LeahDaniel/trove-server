d61f292d055cbd0a8f51718cdc394e3e3bd3c3ee
LeahDaniel <leah.daniel1997@gmail.com>
2022-02-11 16:52:06 -0600

    added handling of IntegrityError on duplicate username entry

--- troveapi/views/auth.py
+++ troveapi/views/auth.py
@@ -1,5 +1,7 @@
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
@@ -12,7 +14,7 @@ def login_user(request):
     '''Handles the authentication of a gamer
 
     Method arguments:
-      request -- The full HTTP request object
+        request -- The full HTTP request object
     '''
     username = request.data['username']
     password = request.data['password']
@@ -42,20 +44,23 @@ def register_user(request):
     '''Handles the creation of a new gamer for authentication
 
     Method arguments:
-      request -- The full HTTP request object
+        request -- The full HTTP request object
     '''
 
-    # Create a new user by invoking the `create_user` helper method
-    # on Django's built-in User model
-    new_user = User.objects.create_user(
-        username=request.data['username'],
-        password=request.data['password'],
-        first_name=request.data['first_name'],
-        last_name=request.data['last_name']
-    )
+    try:
+        # Create a new user by invoking the `create_user` helper method
+        # on Django's built-in User model
+        new_user = User.objects.create_user(
+            username=request.data['username'],
+            password=request.data['password'],
+            first_name=request.data['first_name'],
+            last_name=request.data['last_name']
+        )
 
-    # Use the REST Framework's token generator on the new user account
-    token = Token.objects.create(user=new_user)
-    # Return the token to the client
-    data = {'token': token.key, "userId": new_user.id}
-    return Response(data)
+        # Use the REST Framework's token generator on the new user account
+        token = Token.objects.create(user=new_user)
+        # Return the token to the client
+        data = {'token': token.key, "userId": new_user.id}
+        return Response(data)
+    except IntegrityError as ex:
+        return Response({'message': ex.args[0]}, status=status.HTTP_409_CONFLICT)
