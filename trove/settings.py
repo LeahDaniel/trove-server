3cdfbc31709b35fc26a58c361cbc90c9f2b992ca
LeahDaniel <leah.daniel1997@gmail.com>
2022-02-12 22:28:35 -0600

    forgot a comma

--- trove/settings.py
+++ trove/settings.py
@@ -62,7 +62,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://localhost:5000',
-    'http://127.0.0.1:5000'
+    'http://127.0.0.1:5000',
     'https://trove-leah.herokuapp.com'
 )
 
