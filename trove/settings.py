57eea8070cbca46ebc076ca002332bd332199a49
LeahDaniel <leah.daniel1997@gmail.com>
2022-02-12 22:25:35 -0600

    Cors fix?

--- trove/settings.py
+++ trove/settings.py
@@ -63,7 +63,7 @@ CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:3000',
     'http://localhost:5000',
     'http://127.0.0.1:5000'
-    'https://trove-leah.herokuapp.com/'
+    'https://trove-leah.herokuapp.com'
 )
 
 # UPDATE THIS
