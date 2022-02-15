b31611c86dc6f19256260a4dce09eecc1ba8ac78
LeahDaniel <leah.daniel1997@gmail.com>
2022-02-15 11:33:02 -0600

    added a cors whitelist

--- trove/settings.py
+++ trove/settings.py
@@ -63,7 +63,8 @@ CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:3000',
     'http://localhost:5000',
     'http://127.0.0.1:5000',
-    'https://trove-leah.herokuapp.com'
+    'http://trove-leah.herokuapp.com',
+    'https://trove-leah.herokuapp.com',
 )
 
 # UPDATE THIS
